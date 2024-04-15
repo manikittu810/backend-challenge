b64cfe0450b4a98c0b0daa7e59b17a906fb3161a
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-04-14 18:25:57 -0700

    added test case

--- taskmanager/tasks/tests.py
+++ taskmanager/tasks/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+#test case for home view 
+class SimpleTest(TestCase):
+    def test_home_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content.decode(), "Welcome to the Task Manager API!")
